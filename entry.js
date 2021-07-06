5b5f4d7573fffb6763f31bd6ec876aec12da7a68
Humza Muzammal <humza.muzammal@getfutureproof.co.uk>
2021-07-06 16:39:26 +0100

    styling on entry page (form section) centred gifs, added in interaction bar. started styling new post on home page

--- entry.js
+++ entry.js
@@ -2,6 +2,8 @@
 
 const homeButton = document.getElementById("homeButton");
 
+const journalTitle = document.getElementById("journalTitle");
+
 homeButton.addEventListener("click", goHome);
 
 function goHome() {
@@ -45,11 +47,17 @@ async function postComment(event) {
 async function getJournalWithId(id) {
   let response = await fetch("https://debtomza-server.herokuapp.com/journals");
   let responseJson = await response.json();
-  let journal = responseJson[id-1];
+  let journal = responseJson[id - 1];
   document.title = journal.title;
+  journalTitle.textContent = journal.title;
+
+  // let journalTitle = document.createElement("h2");
+
+  // journalHolder.appendChild(journalTitle);
+
   journalHolder.innerHTML = journal.content;
-  let gifImage = document.createElement('img');
-  gifImage.setAttribute('src', journal.gifUrl);
+  let gifImage = document.createElement("img");
+  gifImage.setAttribute("src", journal.gifUrl);
   journalHolder.appendChild(gifImage);
   let comments = journal.comments;
   comments.forEach((comment) => createComment(comment));
@@ -60,5 +68,3 @@ function createComment(comment) {
   div.textContent = comment;
   commentContainer.appendChild(div);
 }
-
-
